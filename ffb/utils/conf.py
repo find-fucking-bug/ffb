d227f19dbe05c81c668424afa64027bfd7441527
Ali Yaman <aliymn.db@gmail.com>
2024-09-09 16:10:07 +0200

    📚 docs: readme updated

--- ffb/utils/conf.py
+++ ffb/utils/conf.py
@@ -2,7 +2,9 @@ from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
-    OLLAMA_API_URL: str = "http://localhost:11434"
+    OLLAMA_API_URL: str
+    OLLAMA_USER_NAME: str
+    OLLAMA_PASSWORD: str
 
     class Config:
         env_file = ".env"
